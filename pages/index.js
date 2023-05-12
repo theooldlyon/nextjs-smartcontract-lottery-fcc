5ae32545fcb38676eb05ada22ef6e21f8659cbcc
Luca4G1 <Luca4G1@iMac.wind3.hub>
2023-05-12 09:37:10 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head"
+// import Image from "next/image"
+// import { Inter } from "next/font/google"
+import styles from "../styles/Home.module.css"
+import ManualHeader from "../components/ManualHeader"
+import Header from "../components/Header"
+import LotteryEntrance from "../components/LotteryEntrance"
+
+// const inter = Inter({ subsets: ["latin"] })
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Smart Contract Lottery</title>
+                <meta name="description" content="Our Smart Contract Lottery" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            {/* Header /connnect button / nav bar  */}
+            {/* <ManualHeader /> */}
+            <Header />
+            <LotteryEntrance />
+        </>
+    )
+}
